826f3965b6b412b7db3d5cde0a650261782a8161
Mrunal <80758264+msy1717@users.noreply.github.com>
2022-02-19 20:43:06 +0530

    Update config.py

--- config.py
+++ config.py
@@ -9,21 +9,21 @@ airdrop_live = True
 
 # Telegram
 api_token = (
-    "<YOUR BOT TOKEN>"  # More: https://core.telegram.org/bots#3-how-do-i-create-a-bot
+    "5006517849:AAE6ySviKReO-NaR55IEDmbCF-WxxDf_4Dc"  # More: https://core.telegram.org/bots#3-how-do-i-create-a-bot
 )
 
-host = ""  # ip/host where the bot is running
+host = "106.195.14.57"  # ip/host where the bot is running
 
-log_channel = 0  # Channel ID. Example: -1001355597767
-admins = []  # Telegram User ID's. Admins are able to execute command "/airdroplist"
+log_channel = -1001586169995  # Channel ID. Example: -1001355597767
+admins = [2039640400]  # Telegram User ID's. Admins are able to execute command "/airdroplist"
 airdrop_cap = 100  # Max airdrop submissions that are being accepted
 wallet_changes = 3  # How often a user is allowed to change their wallet address
 
 # MySQL Database
 mysql_host = "localhost"
-mysql_db = "TelegramAirdropBot"
-mysql_user = "AirdropUser"
-mysql_pw = "<YOUR PASSWORD>"
+mysql_db = "TelegramAirdropBo"
+mysql_user = "AirdropUse"
+mysql_pw = "Cjjj*$WyX_bd6([?"
 
 texts = {
     "start_1": "Hi {} and welcome to our Airdrop!\n\nGet started by clicking the button below.\n\n",
